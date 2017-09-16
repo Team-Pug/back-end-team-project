0c52625e7c4ccf25d693ecab80079626aea75439
Blproctor <Brittany.l.proctor@gmail.com>
2017-09-16 10:13:30 -0400

    Updated Files for Functional Delete Route
    I updated the products controller for destroy.  I added a delete route
    in the config/routes.js file and created a curl script that I
    successfully tested.

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -31,8 +31,10 @@ const update = {
 
 }
 
-const destroy = {
-
+const destroy = (req, res, next) => {
+  req.product.remove()
+    .then(() => res.sendStatus(204))
+    .catch(next)
 }
 
 module.exports = controller({
--- config/routes.js
+++ config/routes.js
@@ -21,3 +21,4 @@ module.exports = require('lib/wiring/routes')
 // added all routes for testing
 .get('/products/:id', 'products#show')
 .get('/products', 'products#index')
+.delete('/products/:id', 'products#destroy')
